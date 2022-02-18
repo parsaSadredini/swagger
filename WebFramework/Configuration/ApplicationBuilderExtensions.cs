0990a0a6dcf355ddc88d26f7a08e428b2c1afa61
parsa sadredini <roddies9900@gmail.com>
2022-02-18 14:08:39 +0330

    initial commit

---
+++ WebFramework/Configuration/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Common.Utilities;
+using Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebFramework.Configuration
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseHsts(this IApplicationBuilder app, IHostingEnvironment env)
+        {
+            Assert.NotNull(app, nameof(app));
+            Assert.NotNull(env, nameof(env));
+
+            if (!env.IsDevelopment())
+                app.UseHsts();
+        }
+    }
+}
